8da15b09692f45b732331f16d38ad56ab8a9d020
ThickPropheT <ThickPropheT@users.noreply.github.com>
2024-11-09 03:33:50 -0600

    Revert these outdated test harness changes.

--- Reg.Roup.Tests/Resources/TestScenarios/GivenTextMatchedByRegex.cs
+++ Reg.Roup.Tests/Resources/TestScenarios/GivenTextMatchedByRegex.cs
@@ -1,5 +1,4 @@
 ﻿using Reg.Roup.Conversions;
-using Reg.Roup.Expression;
 using System.Linq.Expressions;
 using System.Text.RegularExpressions;
 
@@ -10,10 +9,8 @@ namespace Reg.Roup.Tests.Resources.TestScenarios
         protected readonly Func<string> GetText = getText;
         public Regex Regex { get; } = regex;
 
-        public string _GetText() => GetText();
-
-        public Schema DeserializeMatch<Schema>(Expression<Func<IParse, Schema>> schema, VisitorEngine visitor)
-            => Regex.DeserializeMatch(GetText(), schema, visitor);
+        public Schema DeserializeMatch<Schema>(Expression<Func<IParse, Schema>> schema)
+            => Regex.DeserializeMatch(GetText(), schema);
     }
 
     public class GivenTextMatchedByRegex<TExpected>(TExpected expected, Func<string> getText, Regex regex)
@@ -22,7 +19,6 @@ namespace Reg.Roup.Tests.Resources.TestScenarios
         public TExpected Expected { get; } = expected;
 
         public RegexDeserializationResult<TExpected, Actual> WhenMatchDeserializedVia<Actual>(Expression<Func<IParse, Actual>> schema)
-            //=> new(Expected, DeserializeMatch(schema));
-            => throw new NotImplementedException("Under construction - awaiting final solution to changes above ^^^");
+            => new(Expected, DeserializeMatch(schema));
     }
 }
--- Reg.Roup/RegexExtensions.cs
+++ Reg.Roup/RegexExtensions.cs
@@ -2,7 +2,6 @@
 using System.Linq.Expressions;
 using System.Text.RegularExpressions;
 using Reg.Roup.Conversions;
-using Reg.Roup.Expression;
 using Reg.Roup.Schema;
 
 namespace Reg.Roup
@@ -12,12 +11,8 @@ namespace Reg.Roup
         // TODO
         //  consider renaming.
         //   ? MapGroups
-        public static TSchema DeserializeMatch<TSchema>(this Regex regex, string input, Expression<Func<IParse, TSchema>> deserializationSchema, VisitorEngine visitor)
-        //=> DeserializeTo<TSchema>(new MatchContext(regex, regex.Match(input)), DeserializationSchema.From(deserializationSchema));
-        {
-            var v = visitor.Visit(deserializationSchema);
-            return default;
-        }
+        public static TSchema DeserializeMatch<TSchema>(this Regex regex, string input, Expression<Func<IParse, TSchema>> deserializationSchema)
+            => DeserializeTo<TSchema>(new MatchContext(regex, regex.Match(input)), DeserializationSchema.From(deserializationSchema));
 
         // TODO consider deleting.
         public static TSchema Deserialize<TSchema>(this Match match, Expression<Func<IParse, TSchema>> deserializationSchema, Regex? regex = null)
