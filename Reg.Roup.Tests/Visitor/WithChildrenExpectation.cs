04b920abaeafa68606a5e77f014de4a594cf9a25
ThickPropheT <ThickPropheT@users.noreply.github.com>
2025-01-05 04:12:52 -0600

    Adds test coverage for with-children expectation.

---
+++ Reg.Roup.Tests/Visitor/WithChildrenExpectation.cs
@@ -0,0 +1,44 @@
+﻿namespace Reg.Roup.Tests.Visitor
+{
+    using Reg.Roup.Expression;
+    using System.Linq.Expressions;
+
+    [TestFixture]
+    public class WithChildrenExpectation
+    {
+        private IBaseExpectation expectation;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            expectation = _ExpectExt
+                .NodeType(ExpressionType.Negate)
+                .WithChildren((expression, options) =>
+                    options.NodeType(ExpressionType.Constant)
+                );
+        }
+
+        [Test]
+        public void ThrowsOnInvalidSchemas()
+        {
+            var invalidExpression = Expression.Add(Expression.Constant(1), Expression.Constant(1));
+
+            Assert.That(() => new VisitorEngine(expectation).Visit(invalidExpression), Throws.Exception);
+        }
+
+        [Test]
+        public void PassesThroughValidSchemas()
+        {
+            var validExpression = Expression.Negate(Expression.Constant(69));
+
+            var validatedExpression = new VisitorEngine(expectation).Visit(validExpression);
+
+            Assert.That(validatedExpression, Is.Not.Null);
+
+            var expressionResult = Expression.Lambda(validatedExpression).Compile().DynamicInvoke();
+
+            Assert.That(validatedExpression.NodeType, Is.EqualTo(ExpressionType.Negate));
+            Assert.That(expressionResult, Is.EqualTo(-69));
+        }
+    }
+}
