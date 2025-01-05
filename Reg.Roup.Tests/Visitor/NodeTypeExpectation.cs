d3d789dbe1f43fc237eabaf7ca0d7a6c09ebbc75
ThickPropheT <ThickPropheT@users.noreply.github.com>
2025-01-05 01:18:24 -0600

    Adds coverage for node type validation.

---
+++ Reg.Roup.Tests/Visitor/NodeTypeExpectation.cs
@@ -0,0 +1,35 @@
+﻿using Reg.Roup.Expression;
+
+namespace Reg.Roup.Tests.Visitor
+{
+    using System.Linq.Expressions;
+
+    [TestFixture]
+    public partial class NodeTypeExpectation
+    {
+        [Test]
+        public void ThrowsOnInvalidSchemas()
+        {
+            var invalidSchema = Expression.Add(Expression.Constant(0), Expression.Constant(0));
+            var validation = _ExpectExt.NodeType(ExpressionType.Subtract);
+
+            Assert.That(() => new VisitorEngine(validation).Visit(invalidSchema), Throws.Exception);
+        }
+
+        [Test]
+        public void PassThroughValidSchemas()
+        {
+            var validSchema = Expression.Subtract(Expression.Constant(69), Expression.Constant(27));
+            var validation = _ExpectExt.NodeType(ExpressionType.Subtract);
+
+            var validatedExpression = new VisitorEngine(validation).Visit(validSchema);
+
+            Assert.That(validatedExpression, Is.Not.Null);
+
+            var expressionResult = Expression.Lambda(validatedExpression).Compile().DynamicInvoke();
+
+            Assert.That(validatedExpression.NodeType, Is.EqualTo(ExpressionType.Subtract));
+            Assert.That(expressionResult, Is.EqualTo(42));
+        }
+    }
+}
