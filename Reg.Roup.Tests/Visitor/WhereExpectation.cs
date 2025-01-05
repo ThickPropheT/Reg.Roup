106d557ec6e7c31bd4124d99b4e9a5b71673a3f7
ThickPropheT <ThickPropheT@users.noreply.github.com>
2025-01-05 03:55:18 -0600

    Adds test coverage for where-condition validation.

---
+++ Reg.Roup.Tests/Visitor/WhereExpectation.cs
@@ -0,0 +1,43 @@
+﻿using Reg.Roup.Expression;
+
+namespace Reg.Roup.Tests.Visitor
+{
+    using System.Linq.Expressions;
+
+    [TestFixture]
+    public class WhereExpectation
+    {
+        private IBaseExpectation expectation;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            expectation = _ExpectExt
+                .NodeType<ConstantExpression>()
+                .Where(n => n.Value is int i && i == 69);
+        }
+
+        [Test]
+        public void ThrowsOnInvalidSchemas()
+        {
+            var invalidExpression = Expression.Add(Expression.Constant(1), Expression.Constant(1));
+
+            Assert.That(() => new VisitorEngine(expectation).Visit(invalidExpression), Throws.Exception);
+        }
+
+        [Test]
+        public void PassesThroughValidSchemas()
+        {
+            var validExpression = Expression.Constant(69);
+
+            var validatedExpression = new VisitorEngine(expectation).Visit(validExpression);
+
+            Assert.That(validatedExpression, Is.Not.Null);
+
+            var expressionResult = Expression.Lambda(validatedExpression).Compile().DynamicInvoke();
+
+            Assert.That(validatedExpression.NodeType, Is.EqualTo(ExpressionType.Constant));
+            Assert.That(expressionResult, Is.EqualTo(69));
+        }
+    }
+}
