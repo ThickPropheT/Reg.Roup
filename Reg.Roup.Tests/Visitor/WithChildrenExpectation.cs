3c46996ad6e9be948e38b9c75b89582fb5bd8ab5
Adam Urbas <adam.urbas@fortyau.com>
2025-01-07 01:48:04 -0600

    Cleans up Rider warnings.

--- Reg.Roup.Tests/Visitor/WithChildrenExpectation.cs
+++ Reg.Roup.Tests/Visitor/WithChildrenExpectation.cs
@@ -1,19 +1,19 @@
-﻿namespace Reg.Roup.Tests.Visitor
-{
-    using Reg.Roup.Expression;
-    using System.Linq.Expressions;
+﻿using System.Linq.Expressions;
+using Reg.Roup.Expression;
 
+namespace Reg.Roup.Tests.Visitor
+{
     [TestFixture]
     public class WithChildrenExpectation
     {
-        private IBaseExpectation expectation;
+        private IBaseExpectation _expectation;
 
         [OneTimeSetUp]
         public void OneTimeSetUp()
         {
-            expectation = _ExpectExt
+            _expectation = _ExpectExt
                 .NodeType(ExpressionType.Negate)
-                .WithChildren((expression, options) =>
+                .WithChildren((_, options) =>
                     options.NodeType(ExpressionType.Constant)
                 );
         }
@@ -21,21 +21,21 @@
         [Test]
         public void ThrowsOnInvalidSchemas()
         {
-            var invalidExpression = Expression.Add(Expression.Constant(1), Expression.Constant(1));
+            var invalidExpression = System.Linq.Expressions.Expression.Add(System.Linq.Expressions.Expression.Constant(1), System.Linq.Expressions.Expression.Constant(1));
 
-            Assert.That(() => new VisitorEngine(expectation).Visit(invalidExpression), Throws.Exception);
+            Assert.That(() => new VisitorEngine(_expectation).Visit(invalidExpression), Throws.Exception);
         }
 
         [Test]
         public void PassesThroughValidSchemas()
         {
-            var validExpression = Expression.Negate(Expression.Constant(69));
+            var validExpression = System.Linq.Expressions.Expression.Negate(System.Linq.Expressions.Expression.Constant(69));
 
-            var validatedExpression = new VisitorEngine(expectation).Visit(validExpression);
+            var validatedExpression = new VisitorEngine(_expectation).Visit(validExpression);
 
             Assert.That(validatedExpression, Is.Not.Null);
 
-            var expressionResult = Expression.Lambda(validatedExpression).Compile().DynamicInvoke();
+            var expressionResult = System.Linq.Expressions.Expression.Lambda(validatedExpression).Compile().DynamicInvoke();
 
             Assert.That(validatedExpression.NodeType, Is.EqualTo(ExpressionType.Negate));
             Assert.That(expressionResult, Is.EqualTo(-69));
