c499f202be0d572d39897af19ef4eaa1a375b9b5
Adam Urbas <adam.urbas@fortyau.com>
2025-01-08 01:42:50 -0600

    Fixes usings and other cleanup in these tests.

--- Reg.Roup.Tests/Visitor/WithChildrenExpectation.cs
+++ Reg.Roup.Tests/Visitor/WithChildrenExpectation.cs
@@ -1,44 +1,44 @@
-﻿using System.Linq.Expressions;
-using Reg.Roup.Expression;
+﻿using Reg.Roup.Expression;
 
-namespace Reg.Roup.Tests.Visitor
+namespace Reg.Roup.Tests.Visitor;
+
+using System.Linq.Expressions;
+    
+[TestFixture]
+public class WithChildrenExpectation
 {
-    [TestFixture]
-    public class WithChildrenExpectation
+    private IBaseExpectation _expectation;
+
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
     {
-        private IBaseExpectation _expectation;
+        _expectation = ExpectNode
+            .OfType(ExpressionType.Negate)
+            .WithChildren((_, expect) =>
+                expect.NodeType(ExpressionType.Constant)
+            );
+    }
 
-        [OneTimeSetUp]
-        public void OneTimeSetUp()
-        {
-            _expectation = ExpectNode
-                .OfType(ExpressionType.Negate)
-                .WithChildren((_, options) =>
-                    options.NodeType(ExpressionType.Constant)
-                );
-        }
+    [Test]
+    public void ThrowsOnInvalidSchemas()
+    {
+        var invalidExpression = Expression.Add(Expression.Constant(1), Expression.Constant(1));
 
-        [Test]
-        public void ThrowsOnInvalidSchemas()
-        {
-            var invalidExpression = System.Linq.Expressions.Expression.Add(System.Linq.Expressions.Expression.Constant(1), System.Linq.Expressions.Expression.Constant(1));
+        Assert.That(() => new VisitorEngine(_expectation).Visit(invalidExpression), Throws.Exception);
+    }
 
-            Assert.That(() => new VisitorEngine(_expectation).Visit(invalidExpression), Throws.Exception);
-        }
+    [Test]
+    public void PassesThroughValidSchemas()
+    {
+        var validExpression = Expression.Negate(Expression.Constant(69));
 
-        [Test]
-        public void PassesThroughValidSchemas()
-        {
-            var validExpression = System.Linq.Expressions.Expression.Negate(System.Linq.Expressions.Expression.Constant(69));
+        var validatedExpression = new VisitorEngine(_expectation).Visit(validExpression);
 
-            var validatedExpression = new VisitorEngine(_expectation).Visit(validExpression);
+        Assert.That(validatedExpression, Is.Not.Null);
 
-            Assert.That(validatedExpression, Is.Not.Null);
+        var expressionResult = Expression.Lambda(validatedExpression).Compile().DynamicInvoke();
 
-            var expressionResult = System.Linq.Expressions.Expression.Lambda(validatedExpression).Compile().DynamicInvoke();
-
-            Assert.That(validatedExpression.NodeType, Is.EqualTo(ExpressionType.Negate));
-            Assert.That(expressionResult, Is.EqualTo(-69));
-        }
+        Assert.That(validatedExpression.NodeType, Is.EqualTo(ExpressionType.Negate));
+        Assert.That(expressionResult, Is.EqualTo(-69));
     }
 }
