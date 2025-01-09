254466cb63292e3aba769482e292375fafcc46ba
Adam Urbas <adam.urbas@fortyau.com>
2025-01-08 20:43:47 -0600

    Renames this test fixture for consistency with what it tests.

---
+++ Reg.Roup.Tests/Visitor/OfTypeExpectation.cs
@@ -0,0 +1,40 @@
+﻿using Reg.Roup.Expression;
+
+namespace Reg.Roup.Tests.Visitor;
+
+using System.Linq.Expressions;
+
+[TestFixture]
+public class OfTypeExpectation
+{
+    private IBaseExpectation _expectation;
+
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+        _expectation = ExpectNode.OfType(ExpressionType.Subtract);
+    }
+
+    [Test]
+    public void ThrowsOnInvalidSchemas()
+    {
+        var expression = Expression.Add(Expression.Constant(0), Expression.Constant(0));
+
+        Assert.That(() => new VisitorEngine(_expectation).Visit(expression), Throws.Exception);
+    }
+
+    [Test]
+    public void PassesThroughValidSchemas()
+    {
+        var expression = Expression.Subtract(Expression.Constant(69), Expression.Constant(27));
+
+        var validatedExpression = new VisitorEngine(_expectation).Visit(expression);
+
+        Assert.That(validatedExpression, Is.Not.Null);
+
+        var expressionResult = Expression.Lambda(validatedExpression).Compile().DynamicInvoke();
+
+        Assert.That(validatedExpression.NodeType, Is.EqualTo(ExpressionType.Subtract));
+        Assert.That(expressionResult, Is.EqualTo(42));
+    }
+}
