279479a8fdef9fb12fac36ecf425bdf55c543b6f
Adam Urbas <adam.urbas@fortyau.com>
2025-02-23 04:02:11 -0600

    Refines test coverage for OfType evaluator.

--- TreeVal.Tests/OfType.cs
+++ TreeVal.Tests/OfType.cs
@@ -9,35 +9,17 @@ public class OfType
     [
         Expression.Break(Expression.Label()),
         Expression.Constant(1),
-        Expression.Continue(Expression.Label()),
-        Expression.Default(typeof(int)),
-        // Expression.Dynamic(), // TODO
-        Expression.Empty(),
-        Expression.Goto(Expression.Label()),
         Expression.New(typeof(object).GetConstructor(Type.EmptyTypes)!),
         Expression.Parameter(typeof(int), "index"),
         Expression.Rethrow(),
-        Expression.Return(Expression.Label()),
-        Expression.Variable(typeof(int)),
         Expression.DebugInfo(Expression.SymbolDocument("my.file"), 1, 1, 2, 2),
-        Expression.ClearDebugInfo(Expression.SymbolDocument("my.file")),
-        Expression.Convert(Expression.Constant(1), typeof(short))
+        Expression.Add(Expression.Constant(1), Expression.Constant(1)) 
     ];
     
-    private static readonly Expression[] ValidExpressions =
-    [
-        Expression.Add(Expression.Constant(1), Expression.Constant(1))
-    ];
+    private static readonly Expression ValidExpression = Expression.Default(typeof(int));
     
     private static readonly ExpressionTreeEvaluator Evaluator =
-        ExpressionTreeEvaluator.Create(node =>
-            node
-                .OfType<BinaryExpression>()
-                .HavingChildren(
-                    node.Constant(),
-                    node.Constant()
-                )
-        );
+        ExpressionTreeEvaluator.Create(node => node.OfType<DefaultExpression>());
     
     [TestCaseSource(nameof(InvalidExpressions))]
     public void ThrowsOnInvalidSchemas(Expression invalidExpression)
@@ -45,9 +27,9 @@ public class OfType
         Assert.That(() => Evaluator.Evaluate(invalidExpression), Throws.TypeOf<TreeRejectedException>());
     }
 
-    [TestCaseSource(nameof(ValidExpressions))]
-    public void DoesNotThrowOnValidSchemas(Expression validExpression)
+    [Test]
+    public void DoesNotThrowOnValidSchemas()
     {
-        Assert.That(() => Evaluator.Evaluate(validExpression), Throws.Nothing);
+        Assert.That(() => Evaluator.Evaluate(ValidExpression), Throws.Nothing);
     }
 }
