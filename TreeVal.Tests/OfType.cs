1c2f14a64e99eda7f0c2036fe05095ad90935212
Adam Urbas <adam.urbas@fortyau.com>
2025-02-23 03:54:46 -0600

    Adds test coverage for OfType evaluator.

---
+++ TreeVal.Tests/OfType.cs
@@ -0,0 +1,53 @@
+using System.Linq.Expressions;
+
+namespace TreeVal.Tests;
+
+[TestFixture]
+public class OfType
+{
+    private static readonly Expression[] InvalidExpressions =
+    [
+        Expression.Break(Expression.Label()),
+        Expression.Constant(1),
+        Expression.Continue(Expression.Label()),
+        Expression.Default(typeof(int)),
+        // Expression.Dynamic(), // TODO
+        Expression.Empty(),
+        Expression.Goto(Expression.Label()),
+        Expression.New(typeof(object).GetConstructor(Type.EmptyTypes)!),
+        Expression.Parameter(typeof(int), "index"),
+        Expression.Rethrow(),
+        Expression.Return(Expression.Label()),
+        Expression.Variable(typeof(int)),
+        Expression.DebugInfo(Expression.SymbolDocument("my.file"), 1, 1, 2, 2),
+        Expression.ClearDebugInfo(Expression.SymbolDocument("my.file")),
+        Expression.Convert(Expression.Constant(1), typeof(short))
+    ];
+    
+    private static readonly Expression[] ValidExpressions =
+    [
+        Expression.Add(Expression.Constant(1), Expression.Constant(1))
+    ];
+    
+    private static readonly ExpressionTreeEvaluator Evaluator =
+        ExpressionTreeEvaluator.Create(node =>
+            node
+                .OfType<BinaryExpression>()
+                .HavingChildren(
+                    node.Constant(),
+                    node.Constant()
+                )
+        );
+    
+    [TestCaseSource(nameof(InvalidExpressions))]
+    public void ThrowsOnInvalidSchemas(Expression invalidExpression)
+    {
+        Assert.That(() => Evaluator.Evaluate(invalidExpression), Throws.TypeOf<TreeRejectedException>());
+    }
+
+    [TestCaseSource(nameof(ValidExpressions))]
+    public void DoesNotThrowOnValidSchemas(Expression validExpression)
+    {
+        Assert.That(() => Evaluator.Evaluate(validExpression), Throws.Nothing);
+    }
+}
