19da29f122bdbf2b7714b216d151b2c78ad7db60
Adam Urbas <adam.urbas@fortyau.com>
2025-08-26 10:07:48 -0500

    Note to self.

--- TreeVal/Extensions/ConstructorEvaluatorExtensions.cs
+++ TreeVal/Extensions/ConstructorEvaluatorExtensions.cs
@@ -2,6 +2,9 @@ using System.Linq.Expressions;
 
 namespace TreeVal.Extensions;
 
+// TODO
+//  refine this api and add tests.
+//  add overloads that expose the options supported.
 public static class ConstructorEvaluatorExtensions
 {
     public static IEvaluatorBuilder<NewExpression> New(this IVisitorNodeFactory factory)
