c651ecdc1f0036a6a2cd5e4bc7dff843170058e8
Adam Urbas <adam.urbas@fortyau.com>
2025-09-04 01:33:14 -0500

    Improve usability of Debug condition.

--- TreeVal/Eval.Debug/DebugEvaluatorExtensions.cs
+++ TreeVal/Eval.Debug/DebugEvaluatorExtensions.cs
@@ -1,25 +1,63 @@
+using TreeVal.Eval.Condition;
+using TreeVal.Expr;
+using TreeVal.Media;
 using TreeVal.Scaffolding;
 
 namespace TreeVal.Eval.Debug;
 
 public static class DebugEvaluatorExtensions
 {
-    public static IEvaluatorBuilder Debug(this IEvaluatorBuilderFactory _, Action<object, IConditionEvaluation> observe)
-    {
-        var builder = new EvaluatorBuilder();
-        builder.AddCondition(new Observer(observe));
-        return builder;
-    }
-
     public static IEvaluatorBuilder Debug(this IEvaluatorBuilder builder, Action<object, IConditionEvaluation> observe)
     {
         builder.AddCondition(new Observer(observe));
         return builder;
     }
-    
-    public static IEvaluatorBuilder<T> Debug<T>(this IEvaluatorBuilder<T> builder, Action<T, IConditionEvaluation> observe)
+
+    public static IEvaluatorBuilder<T> Debug<T>(
+        this IEvaluatorBuilder<T> builder, Action<T, IConditionEvaluation> observe)
     {
         builder.AddCondition(new Observer<T>(observe));
         return builder;
+    }
+
+    public static IEvaluatorBuilderFactory Debug(
+        this IEvaluatorBuilderFactory factory, Action<object, IConditionEvaluation> observe)
+        => new BuilderFactoryAspect(
+            factory,
+            evaluator => new AttachDebuggerEvaluator(
+                evaluator,
+                () =>
+                    factory
+                        .AnyOne()
+                        .Debug((o, evaluation) => observe(o, evaluation))));
+
+    private class AttachDebuggerEvaluator : INodeEvaluator
+    {
+        private readonly INodeEvaluator _target;
+        private readonly Func<INodeEvaluatorFactory> _buildDebugEvaluator;
+
+        public IEnumerable<ICondition> Conditions => _target.Conditions;
+
+        public VisitationContext.MovementStrategy HeadMovementStrategy => _target.HeadMovementStrategy;
+
+        public VisitationContext.EvaluationStrategy? ChildEvaluationStrategy => _target.ChildEvaluationStrategy;
+
+        public AttachDebuggerEvaluator(INodeEvaluator target, Func<INodeEvaluatorFactory> buildDebugEvaluator)
+        {
+            _target = target;
+            _buildDebugEvaluator = buildDebugEvaluator;
+        }
+
+        public IEnumerable<INodeEvaluatorFactory> EnumerateChildren(Node current)
+        {
+            yield return _buildDebugEvaluator();
+
+            foreach (var childBuilder in _target.EnumerateChildren(current))
+            {
+                yield return childBuilder;
+            }
+
+            ;
+        }
     }
 }
